0700906a87f64869c091b87df7a4c905ce8375b9
Simon Michalke <simon.m.m@freenet.de>
2014-05-23 20:38:26 +0200

    added the thread garbage collector; Logging.cpp and Logging.h

--- vesper_log/Logging.cpp
+++ vesper_log/Logging.cpp
@@ -1,4 +1,111 @@
-#include "Logging.H"
+#include "Logging.h"
 
 using namespace Vesper;
+
+int Logging::nextID = 1;
+
+//initialize the garbageCollector
+Logging::garbageCollectorDataStruct Logging::garbageCollectorData =
+{
+    new std::mutex(),
+    0,
+    false
+};
+
+Logging::Logging()
+{
+    garbageCollectorData.threadRunning = true;
+    garbageCollectorData.gStack = 0;
+
+    uniqueID = 0;
+
+    //start the thread:
+    garbageCollectorThread = new std::thread(Logging::startGarbageCollector, this);
+}
+
+Logging::Logging(LoggingType::LoggingClientType typets)
+{
+    clientType = typets;
+}
+
+Logging::~Logging()
+{
+
+}
+
+int Logging::getID()
+{
+    return uniqueID;
+}
+
+void Logging::logStart(char *text, ...)
+{
+}
+
+void Logging::logDebug(char *text, ...)
+{
+}
+
+void Logging::logNeutral(char *text, ...)
+{
+}
+
+void Logging::logWarning(char *text, ...)
+{
+}
+
+void Logging::logError(char *text, ...)
+{
+}
+
+void Logging::startGarbageCollector(Logging *toStart)
+{
+    toStart->garbageCollector();
+}
+
+void Logging::garbageCollector()
+{
+    logNeutral("Garbage Collector for Logging started!");
+    while (garbageCollectorData.threadRunning)
+    {
+        //we are now collecting garbage, so please don't disturb us!
+        garbageCollectorData.locker->lock();
+
+        while (garbageCollectorData.gStack)
+        {//pop the threads from stack
+            LoggingType::garbageStackItem *structToDelete = garbageCollectorData.gStack;
+            delete(structToDelete->toDelete);
+            garbageCollectorData.gStack = structToDelete->next;
+            delete(structToDelete);
+        }
+
+        //unlock the Struct:
+        garbageCollectorData.locker->unlock();
+        sleep(1); // don't take too much CPU!
+    }
+}
+
+void Logging::stopGarbageCollector()
+{
+    garbageCollectorData.threadRunning = false;
+}
+
+void Logging::deleteThread(std::thread *toDelete)
+{
+    //we are now deleting a thread so please don't disturb us!
+    garbageCollectorData.locker->lock();
+
+    LoggingType::garbageStackItem *oldTop;
+    oldTop = garbageCollectorData.gStack;
+
+    //create our new Item:
+    garbageCollectorData.gStack = new LoggingType::garbageStackItem
+                                      {
+                                      toDelete,
+                                      oldTop
+                                      };
+
+    //unlock the Struct:
+    garbageCollectorData.locker->unlock();
+}
 
--- vesper_log/Logging.h
+++ vesper_log/Logging.h
@@ -1,20 +1,122 @@
+#ifndef LOGGING_H_INCLUDED
+#define LOGGING_H_INCLUDED
+
 #include "Vout.h"
 
-namespace Vesper{
+#include <iostream>
+#include <thread>
+
+namespace Vesper
+{
+
+/**
+ * The namespace LoggingTypes will contain enums, structs
+ * and other things, that are needed by the Logging class
+ * We use a namespace to avoid Name-conflicts
+ * --> for example use this: LoggingType.client
+ * --> avoid using the LoggingType namespace
+ */
+namespace LoggingType
+{
+
+enum LoggingClientType
+{
+    client, //
+    server
+};
+
+struct garbageStackItem
+{
+    std::thread *toDelete;
+    garbageStackItem *next;
+};
+
+}; /* namespace LoggingType*/
 
 class Logging
 {
     public:
 
+        /**
+         * Invoke the default constructor only once
+         * It will initialize the garbageCollector
+         * in general and start the thread
+         */
         Logging();
+        Logging(LoggingType::LoggingClientType typets);
         ~Logging();
 
+        int getID();
+
+        void logStart(char *text, ...);
+        void logDebug(char *text, ...);
+        void logNeutral(char *text, ...);
+        void logWarning(char *text, ...);
+        void logError(char *text, ...);
+
+        /**
+         * This is the actual GarbageCollector. It
+         * sleeps for 1 second and then clears the stack.
+         * It checks for the mutex if its locked!
+         */
+        void garbageCollector();
+
+        /**
+         * startGarbageCollector() just starts the
+         * garbageCollector. We need a static function
+         * for our thread.
+         */
+        static void startGarbageCollector(Logging *toStart);
+
+        /**
+         * Stops the garbageCollector
+         */
+        void stopGarbageCollector();
 
     protected:
     private:
+
+        LoggingType::LoggingClientType clientType;
+
+        /**
+         * The unique identification that will be
+         * given to the next client
+         */
+        static int nextID;
+
+        /**
+         * The unique identification
+         * the uID 0 is the garbageCollector class
+         * itself
+         */
+        int uniqueID;
+
+        /* We may need this ...
+        void print(char *data);
+        void printLn(char *data);
+        void printLnEnd();*/
+
+        std::thread *garbageCollectorThread;
+
+        struct garbageCollectorDataStruct{
+            std::mutex *locker;
+            LoggingType::garbageStackItem *gStack;
+
+            bool threadRunning;
+        }static garbageCollectorData;
+
+        /**
+         * After the thread wrote something it invokes
+         * deleteThread. deleteThread puts the thread
+         * on the garbageCollectorStack.
+         * The garbageCollectorThread automatically
+         * the "garbage" after max. 1 second.
+         */
+        void deleteThread(std::thread *toDelete);
 
 
 };
 
 }; /* namespace Vesper */
 
+#endif /* LOGGING_H_INCLUDED */
