eca2b196aefa55b42557795f6c84f4292aab5c64
jellysheep <max.mail@dameweb.de>
2014-05-28 14:35:02 +0200

    Added basic SphTP implementation structure.

---
+++ vesper_sphtp/network_connector.h
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2014, Max Mertens. All rights reserved.
+ * This file is licensed under the "BSD 3-Clause License".
+ * Full license text is under the file "LICENSE" provided with this code.
+ */
+
+#if !defined NETWORK_CONNECTOR_H_INCLUDED
+#define NETWORK_CONNECTOR_H_INCLUDED
+
+#endif /* !defined NETWORK_CONNECTOR_H_INCLUDED */
