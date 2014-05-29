7ee57a41578dbf53b96f38a81db3ffd9308e79a8
jellysheep <max.mail@dameweb.de>
2014-05-29 12:40:05 +0200

    Added SphTP test to vesper-test.

--- vesper_test/vsp_test.c
+++ vesper_test/vsp_test.c
@@ -8,7 +8,27 @@
 
 #include <vsp_network_connector.h>
 
+MU_TEST(vesper_sphtp_allocation)
+{
+    struct vsp_network_connector *net_conn;
+    int ret;
+    /* test allocation */
+    net_conn = vsp_network_connector_new();
+    mu_check(net_conn != NULL);
+    /* test deallocation */
+    ret = vsp_network_connector_close(net_conn);
+    mu_check(ret == 0);
+}
+
+/* test SphTP implementation */
+MU_TEST_SUITE(vesper_sphtp)
+{
+    MU_RUN_TEST(vesper_sphtp_allocation);
+}
+
 int main(void)
 {
+    MU_RUN_SUITE(vesper_sphtp);
+    MU_REPORT();
     return 0;
 }
