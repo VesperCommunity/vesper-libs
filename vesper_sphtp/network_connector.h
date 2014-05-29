466ffe20e2c9241ac1b0fb4d09c8e6784fcb1c22
jellysheep <max.mail@dameweb.de>
2014-05-29 10:26:50 +0200

    Merged header files into network_connector.h.

--- vesper_sphtp/network_connector.h
+++ vesper_sphtp/network_connector.h
@@ -7,4 +7,33 @@
 #if !defined NETWORK_CONNECTOR_H_INCLUDED
 #define NETWORK_CONNECTOR_H_INCLUDED
 
+#if defined __cplusplus
+extern "C" {
+#endif /* defined __cplusplus */
+
+/* only export API functions prefixed with SPHTP_API */
+#if defined _WIN32
+  #if defined SPHTP_BUILD_API
+    /* build dll */
+    #define SPHTP_API __declspec(dllexport)
+  #else
+    /* use dll */
+    #define SPHTP_API __declspec(dllimport)
+  #endif /* defined SPHTP_BUILD_API*/
+#else
+  #if __GNUC__ >= 4
+    #define SPHTP_API __attribute__((visibility("default")))
+  #else
+    #define SPHTP_API
+  #endif
+#endif /* defined _WIN32 */
+
+
+
+#undef SPHTP_API
+
+#if defined __cplusplus
+}
+#endif /* defined __cplusplus */
+
 #endif /* !defined NETWORK_CONNECTOR_H_INCLUDED */
