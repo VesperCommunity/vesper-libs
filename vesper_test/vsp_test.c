e55afa50231a594df861d0bbfd2d931bf3e0572b
jellysheep <max.mail@dameweb.de>
2014-07-28 18:36:47 +0200

    Added basic CMCP connection tests.

--- vesper_test/vsp_test.c
+++ vesper_test/vsp_test.c
@@ -10,33 +10,52 @@
 #include "minunit.h"
 
 #include <vesper_util/vsp_error.h>
+#include <vesper_cmcp/vsp_cmcp_server.h>
 #include <vesper_cmcp/vsp_cmcp_client.h>
 
-/** Publish socket address. */
-#define PUBLISH_ADDRESS "tcp://127.0.0.1:7571"
-/** Subscribe socket address. */
-#define SUBSCRIBE_ADDRESS "tcp://127.0.0.1:7572"
+/** Server publish socket address. */
+#define SERVER_PUBLISH_ADDRESS "tcp://127.0.0.1:7571"
+/** Server subscribe socket address. */
+#define SERVER_SUBSCRIBE_ADDRESS "tcp://127.0.0.1:7572"
+
+/** Global CMCP server object. */
+static vsp_cmcp_server *global_cmcp_server;
 
 /** Global CMCP client object. */
 static vsp_cmcp_client *global_cmcp_client;
 
-/** Create global global_cmcp_client object. \see global_cmcp_client */
-void vsp_cmcp_client_setup(void);
-/** Free global global_cmcp_client object. \see global_cmcp_client */
-void vsp_cmcp_client_teardown(void);
+/** Create global global_cmcp_server and global_cmcp_client objects. */
+void vsp_cmcp_connection_setup(void);
 
-void vsp_cmcp_client_setup(void)
+/** Free global global_cmcp_server and global_cmcp_client objects. */
+void vsp_cmcp_connection_teardown(void);
+
+void vsp_cmcp_connection_setup(void)
 {
+    global_cmcp_server = vsp_cmcp_server_create();
     global_cmcp_client = vsp_cmcp_client_create();
 }
 
-void vsp_cmcp_client_teardown(void)
+void vsp_cmcp_connection_teardown(void)
 {
     vsp_cmcp_client_free(global_cmcp_client);
+    vsp_cmcp_server_free(global_cmcp_server);
 }
 
-/** Test vsp_cmcp_client_create() and
- * vsp_cmcp_client_free(). */
+/** Test vsp_cmcp_server_create() and vsp_cmcp_server_free(). */
+MU_TEST(vsp_cmcp_server_allocation)
+{
+    vsp_cmcp_server *local_global_cmcp_server;
+    int ret;
+    /* allocation */
+    local_global_cmcp_server = vsp_cmcp_server_create();
+    mu_assert(local_global_cmcp_server != NULL, vsp_error_str(vsp_error_num()));
+    /* deallocation */
+    ret = vsp_cmcp_server_free(local_global_cmcp_server);
+    mu_assert(ret == 0, vsp_error_str(vsp_error_num()));
+}
+
+/** Test vsp_cmcp_client_create() and vsp_cmcp_client_free(). */
 MU_TEST(vsp_cmcp_client_allocation)
 {
     vsp_cmcp_client *local_global_cmcp_client;
@@ -49,36 +68,40 @@ MU_TEST(vsp_cmcp_client_allocation)
     mu_assert(ret == 0, vsp_error_str(vsp_error_num()));
 }
 
-/** Test vsp_cmcp_client_connect() and subsequent
- * vsp_cmcp_client_disconnect(). */
-MU_TEST(vsp_cmcp_client_reconnection)
+/** Test connection between vsp_cmcp_server and vsp_cmcp_client. */
+MU_TEST(vsp_cmcp_connection)
 {
     int ret;
-    /* connection */
-    ret = vsp_cmcp_client_connect(global_cmcp_client, PUBLISH_ADDRESS,
-        SUBSCRIBE_ADDRESS);
+    /* bind */
+    ret = vsp_cmcp_server_bind(global_cmcp_server, SERVER_PUBLISH_ADDRESS,
+        SERVER_SUBSCRIBE_ADDRESS);
     mu_assert(ret == 0, vsp_error_str(vsp_error_num()));
-    /* disconnection */
+    /* connect */
+    ret = vsp_cmcp_client_connect(global_cmcp_client, SERVER_SUBSCRIBE_ADDRESS,
+        SERVER_PUBLISH_ADDRESS);
+    mu_assert(ret == 0, vsp_error_str(vsp_error_num()));
+    /* disconnect */
     ret = vsp_cmcp_client_disconnect(global_cmcp_client);
     mu_assert(ret == 0, vsp_error_str(vsp_error_num()));
-    /* reconnection */
-    ret = vsp_cmcp_client_connect(global_cmcp_client, PUBLISH_ADDRESS,
-        SUBSCRIBE_ADDRESS);
+    /* unbind */
+    ret = vsp_cmcp_server_unbind(global_cmcp_server);
     mu_assert(ret == 0, vsp_error_str(vsp_error_num()));
 }
 
 /** Test CMCP implementation. */
-MU_TEST_SUITE(vesper_cmcp_client)
+MU_TEST_SUITE(vsp_cmcp_tests)
 {
-    MU_SUITE_CONFIGURE(&vsp_cmcp_client_setup, &vsp_cmcp_client_teardown);
-    MU_RUN_TEST(vsp_cmcp_client_reconnection);
+    MU_SUITE_CONFIGURE(&vsp_cmcp_connection_setup,
+        &vsp_cmcp_connection_teardown);
+    MU_RUN_TEST(vsp_cmcp_connection);
 }
 
 /** Run all module tests. */
 int main(void)
 {
+    MU_RUN_TEST(vsp_cmcp_server_allocation);
     MU_RUN_TEST(vsp_cmcp_client_allocation);
-    MU_RUN_SUITE(vesper_cmcp_client);
+    MU_RUN_SUITE(vsp_cmcp_tests);
     MU_REPORT();
     return 0;
 }
