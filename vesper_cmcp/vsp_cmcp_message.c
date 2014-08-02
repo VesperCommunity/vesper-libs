39d60de622482686e032bdb5c04e9d34dabfe614
jellysheep <max.mail@dameweb.de>
2014-08-02 08:44:01 +0200

    Added basic CMCP message implementation.

---
+++ vesper_cmcp/vsp_cmcp_message.c
@@ -0,0 +1,50 @@
+/**
+ * \authors Max Mertens
+ *
+ * Copyright (c) 2014, Max Mertens. All rights reserved.
+ * This file is licensed under the "BSD 3-Clause License".
+ * Full license text is under the file "LICENSE" provided with this code.
+ */
+
+#include "vsp_cmcp_message.h"
+
+#include <vesper_util/vsp_error.h>
+#include <vesper_util/vsp_util.h>
+#include <stdlib.h>
+
+/** Message data sent over (network) connection. */
+struct vsp_cmcp_message {
+    /** Message topic or receiver ID. */
+    uint16_t topic_id;
+    /** Message sender ID. */
+    uint16_t sender_id;
+    /** Message command ID. */
+    uint16_t command_id;
+    /** Message parameters stored as data list items. */
+    vsp_cmcp_datalist *cmcp_datalist;
+};
+
+vsp_cmcp_message *vsp_cmcp_message_create(uint16_t topic_id,
+    uint16_t sender_id, uint16_t command_id, vsp_cmcp_datalist *cmcp_datalist)
+{
+    vsp_cmcp_message *cmcp_message;
+    /* allocate memory */
+    VSP_ALLOC(cmcp_message, vsp_cmcp_message, return NULL);
+    /* initialize struct data */
+    cmcp_message->topic_id = topic_id;
+    cmcp_message->sender_id = sender_id;
+    cmcp_message->command_id = command_id;
+    cmcp_message->cmcp_datalist = cmcp_datalist;
+    /* return struct pointer */
+    return cmcp_message;
+}
+
+int vsp_cmcp_message_free(vsp_cmcp_message *cmcp_message)
+{
+    /* check parameter */
+    VSP_ASSERT(cmcp_message != NULL, vsp_error_set_num(EINVAL); return -1);
+
+    /* free memory */
+    VSP_FREE(cmcp_message);
+    return 0;
+}
---
+++ vesper_cmcp/vsp_cmcp_message.h
@@ -0,0 +1,51 @@
+/**
+ * \file
+ * \authors Max Mertens
+ *
+ * Copyright (c) 2014, Max Mertens. All rights reserved.
+ * This file is licensed under the "BSD 3-Clause License".
+ * Full license text is under the file "LICENSE" provided with this code.
+ */
+
+#if !defined VSP_CMCP_MESSAGE_H_INCLUDED
+#define VSP_CMCP_MESSAGE_H_INCLUDED
+
+#include "vsp_cmcp_datalist.h"
+
+#include <vesper_util/vsp_api.h>
+#include <stdint.h>
+
+#if defined __cplusplus
+extern "C" {
+#endif /* defined __cplusplus */
+
+#define VSP_CMCP_MESSAGE_HEADER_LENGTH 6
+
+/** Message data sent over (network) connection. */
+struct vsp_cmcp_message;
+
+/** Define type vsp_cmcp_message to avoid 'struct' keyword. */
+typedef struct vsp_cmcp_message vsp_cmcp_message;
+
+/**
+ * Create new vsp_cmcp_message object.
+ * Returned pointer should be freed with vsp_cmcp_message_free().
+ * The vsp_cmcp_message will not free the vsp_cmcp_datalist object and it has
+ * to be accessible until vsp_cmcp_message_free() is called.
+ * Returns NULL and sets vsp_error_num() if failed.
+ */
+VSP_API vsp_cmcp_message *vsp_cmcp_message_create(uint16_t topic_id,
+    uint16_t sender_id, uint16_t command_id, vsp_cmcp_datalist *cmcp_datalist);
+
+/**
+ * Free vsp_cmcp_message object.
+ * Object should be created with vsp_cmcp_message_create().
+ * Returns non-zero and sets vsp_error_num() if failed.
+ */
+VSP_API int vsp_cmcp_message_free(vsp_cmcp_message *cmcp_message);
+
+#if defined __cplusplus
+}
+#endif /* defined __cplusplus */
+
+#endif /* !defined VSP_CMCP_MESSAGE_H_INCLUDED */
