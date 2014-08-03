60c780ac19910fb02d979fde9fff3bfe8a80087e
jellysheep <max.mail@dameweb.de>
2014-08-03 10:22:16 +0200

    Improved cmcp_message test method to validate parsed data.

--- vesper_test/vsp_test_cmcp_message.c
+++ vesper_test/vsp_test_cmcp_message.c
@@ -15,6 +15,7 @@
 #include <vesper_util/vsp_error.h>
 #include <vesper_util/vsp_util.h>
 #include <stdio.h>
+#include <string.h>
 
 /** Create message and test creating binary data and parse it. */
 MU_TEST(vsp_test_cmcp_message_test);
@@ -27,6 +28,7 @@ MU_TEST(vsp_test_cmcp_message_test)
     int data_length;
     void *data_pointer;
     void *data_item_pointer;
+    uint16_t message_id;
 
     /* allocate data list */
     cmcp_datalist1 = vsp_cmcp_datalist_create();
@@ -60,6 +62,37 @@ MU_TEST(vsp_test_cmcp_message_test)
     /* construct second message using binary data array */
     cmcp_message2 = vsp_cmcp_message_create_parse(data_length, data_pointer);
     mu_assert(cmcp_message2 != NULL, vsp_error_str(vsp_error_num()));
+
+    /* get back and verify message IDs */
+    ret = vsp_cmcp_message_get_id(cmcp_message2, VSP_CMCP_MESSAGE_TOPIC_ID,
+        &message_id);
+    mu_assert(ret == 0, vsp_error_str(vsp_error_num()));
+    mu_assert(message_id == MESSAGE_TOPIC_ID, vsp_error_str(EINVAL));
+    ret = vsp_cmcp_message_get_id(cmcp_message2, VSP_CMCP_MESSAGE_SENDER_ID,
+        &message_id);
+    mu_assert(ret == 0, vsp_error_str(vsp_error_num()));
+    mu_assert(message_id == MESSAGE_SENDER_ID, vsp_error_str(EINVAL));
+    ret = vsp_cmcp_message_get_id(cmcp_message2, VSP_CMCP_MESSAGE_COMMAND_ID,
+        &message_id);
+    mu_assert(ret == 0, vsp_error_str(vsp_error_num()));
+    mu_assert(message_id == MESSAGE_COMMAND_ID, vsp_error_str(EINVAL));
+
+    /* get data list from second message */
+    cmcp_datalist2 = vsp_cmcp_message_get_datalist(cmcp_message2);
+    mu_assert(cmcp_datalist2 != NULL, vsp_error_str(vsp_error_num()));
+
+    /* get back data list item and verify data */
+    data_item_pointer = vsp_cmcp_datalist_get_data_item(cmcp_datalist2,
+        DATALIST_ITEM1_ID, DATALIST_ITEM1_LENGTH);
+    mu_assert(data_item_pointer != NULL, vsp_error_str(vsp_error_num()));
+    mu_assert(memcmp(data_item_pointer, DATALIST_ITEM1_DATA,
+        DATALIST_ITEM1_LENGTH) == 0, vsp_error_str(EINVAL));
+
+    data_item_pointer = vsp_cmcp_datalist_get_data_item(cmcp_datalist2,
+        DATALIST_ITEM2_ID, DATALIST_ITEM2_LENGTH);
+    mu_assert(data_item_pointer != NULL, vsp_error_str(vsp_error_num()));
+    mu_assert(memcmp(data_item_pointer, DATALIST_ITEM2_DATA,
+        DATALIST_ITEM2_LENGTH) == 0, vsp_error_str(EINVAL));
 
     /* deallocation; cmcp_datalist2 will be freed by cmcp_message2 */
     VSP_FREE(data_pointer);
